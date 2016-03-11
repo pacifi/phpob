924b5d124f47b96d0948bf91619006a97aff6117
pacifi <pacifi.bnr@gmail.com>
2016-03-11 16:46:43 -0500

    2

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 	define('DS', DIRECTORY_SEPARATOR);
 	define('ROOT', realpath(dirname(__FILE__)) . DS);
-	define('URL', "http://localhost/proyecto/");
+	define('URL', "http://pacifi.pe.hu/");
 
 	require_once "Config/Autoload.php";
 	Config\Autoload::run();
